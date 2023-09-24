30966699c743825577635eee25d9ae4de9b83d31
valbuilds <katie@katiebuilds.website>
2023-09-24 12:25:46 -0500

    updated embed parsing

--- commands/embeds.py
+++ commands/embeds.py
@@ -1,41 +1,21 @@
 import discord
 from discord import app_commands
-from typing import Optional
-import settings
+import json
 
 class Embeds(app_commands.Group):
-    @app_commands.command(description="Make a custom embed.")
-    async def make(self, interaction: discord.Interaction, title: str, description: Optional[str], field1_name: Optional[str], field1_value: Optional[str], field2_name: Optional[str], field2_value: Optional[str], field3_name: Optional[str], field3_value: Optional[str], field4_name: Optional[str], field4_value: Optional[str], field5_name: Optional[str], field5_value: Optional[str], inlinefields: Optional[bool] = True):
-        embed = discord.Embed(color=discord.Color.blurple())
-        embed.title = title
-        embed.description = description
-        if field1_name is not None and field1_value is not None:
-            embed.add_field(name=field1_name, value=field1_value, inline=inlinefields)
-        if field2_name is not None and field2_value is not None:
-            embed.add_field(name=field2_name, value=field2_value, inline=inlinefields)
-        if field3_name is not None and field3_value is not None:
-            embed.add_field(name=field3_name, value=field3_value, inline=inlinefields)
-        if field4_name is not None and field4_value is not None:
-            embed.add_field(name=field4_name, value=field4_value, inline=inlinefields)
-        if field5_name is not None and field5_value is not None:
-            embed.add_field(name=field5_name, value=field5_value, inline=inlinefields)
-        
-        return await interaction.response.send_message(embed=embed)
-    
-    @app_commands.command(description="Send a pre-made embed!")
-    async def send_premade(self, interaction: discord.Interaction, premadeid: str):
-        a = settings.CONFIG['embeds'][premadeid]
-        embed = discord.Embed()
-        if a['title'] is not None:
-            embed.title = a['title']
-        if a['description'] is not None:
-            embed.description = a['description']
-        if a['color'] is not None:
-            embed.color = discord.Color.from_str(a['color'])
-        elif a['color'] is None:
-            embed.color = discord.Colour.blurple()
+    @app_commands.command()
+    async def send(self, interaction: discord.Interaction, id: str):
+        f = open(f'./config/messages/{id}.json')
+        data = json.load(f)
 
-        return await interaction.response.send_message(embed=embed)
+        e = []
+        for embed in data['embeds']:
+            e.append(discord.Embed.from_dict(embed))
+
+        if interaction.user.guild_permissions.mention_everyone is True:
+            await interaction.response.send_message(content=data['content'], embeds=e, allowed_mentions=discord.AllowedMentions.all())
+        else:
+            await interaction.response.send_message(content=data['content'], embeds=e, allowed_mentions=discord.AllowedMentions.none())
 
 async def setup(bot):
     bot.tree.add_command(Embeds(name="embed", description="Embed commands (version 0.0.1a)"))
--- settings.py
+++ settings.py
@@ -6,27 +6,31 @@ import discord
 
 TOKEN: str = os.getenv('token')
 
-cfile = open('config.json')
+cfile = open('config/config.json')
 CONFIG = json.load(cfile)
 
-ACTIVITY = None
-if CONFIG['bot']['status']['activity']['type'] == 0 or CONFIG['bot']['status']['activity']['type'].lower() == "playing":
-    ACTIVITY = discord.Game(name=CONFIG['bot']['status']['activity']['name'])
-elif CONFIG['bot']['status']['activity']['type'] == 1 or CONFIG['bot']['status']['activity']['type'].lower() == "streaming":
-    ACTIVITY = discord.Streaming(name=CONFIG['bot']['status']['activity']['name'])
-elif CONFIG['bot']['status']['activity']['type'] == 2 or CONFIG['bot']['status']['activity']['type'].lower() == "listening":
-    ACTIVITY = discord.Activity(type=discord.ActivityType.listening, name=CONFIG['bot']['status']['activity']['name'])
-elif CONFIG['bot']['status']['activity']['type'] == 3 or CONFIG['bot']['status']['activity']['type'].lower() == "watching":
-    ACTIVITY = discord.Activity(type=discord.ActivityType.watching, name=CONFIG['bot']['status']['activity']['name'])
-elif CONFIG['bot']['status']['activity']['type'] == 4 or CONFIG['bot']['status']['activity']['type'].lower() == "custom":
-    ACTIVITY = discord.CustomActivity(name=CONFIG['bot']['status']['activity']['name'])
+ACTIVITY = discord.Activity(type=discord.ActivityType.listening, name=f"{CONFIG['bot']['prefix']}help")
+if CONFIG['bot']['status']['activity']['type'] == "playing":
+    ACTIVITY = discord.Activity(type=discord.ActivityType.playing, name=f"{CONFIG['bot']['status']['activity']['name']}")
+elif CONFIG['bot']['status']['activity']['type'] == "streaming":
+    ACTIVITY = discord.Activity(type=discord.ActivityType.streaming, name=f"{CONFIG['bot']['status']['activity']['name']}")
+elif CONFIG['bot']['status']['activity']['type'] == "listening":
+    ACTIVITY = discord.Activity(type=discord.ActivityType.listening, name=f"{CONFIG['bot']['status']['activity']['name']}")
+elif CONFIG['bot']['status']['activity']['type'] == "watching":
+    ACTIVITY = discord.Activity(type=discord.ActivityType.watching, name=f"{CONFIG['bot']['status']['activity']['name']}")
+elif CONFIG['bot']['status']['activity']['type'] == "custom":
+    ACTIVITY = discord.Activity(type=discord.ActivityType.custom, name=f"{CONFIG['bot']['status']['activity']['name']}")
+elif CONFIG['bot']['status']['activity']['type'] == "competing":
+    ACTIVITY = discord.Activity(type=discord.ActivityType.competing, name=f"{CONFIG['bot']['status']['activity']['name']}")
 
 STATUS = discord.Status.online
-if CONFIG['bot']['status']['type'] == 0 or CONFIG['bot']['status']['type'] == "online":
+if CONFIG['bot']['status']['type'] == "online":
     STATUS = discord.Status.online
-elif CONFIG['bot']['status']['type'] == 1 or CONFIG['bot']['status']['type'] == "idle":
+elif CONFIG['bot']['status']['type'] == "idle":
     STATUS = discord.Status.idle
-elif CONFIG['bot']['status']['type'] == 2 or CONFIG['bot']['status']['type'] == "dnd":
+elif CONFIG['bot']['status']['type'] == "dnd":
     STATUS = discord.Status.dnd
-elif CONFIG['bot']['status']['type'] == 3 or CONFIG['bot']['status']['type'] == "invisible":
+elif CONFIG['bot']['status']['type'] == "do not disturb":
+    STATUS = discord.Status.do_not_disturb
+elif CONFIG['bot']['status']['type'] == "invisible":
     STATUS = discord.Status.invisible
